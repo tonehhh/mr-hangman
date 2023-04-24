76d2212e2e2f2f359916134f4b05bcd5f81a9f02
tonehhh <110231836+tonehhh@users.noreply.github.com>
2023-04-24 13:38:25 +0100

    added main structure of hangman game

--- run.py
+++ run.py
@@ -1,3 +1,67 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import word_list
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_completion = "_" len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Let's play Mr. Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You've already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job," guess, "is in the word!")
+                guessed_letters.append(guess)
+                word-as_list = list(word_completion)
+                indicies = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indicies:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+                
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You've already guessed teh word", guess)
+            elif guess != word:
+                print(guess, "is not in the word")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word-completion = word
+
+        else:
+            print("Nota  valid guess.")
+            print(display_hangman(tries))
+            print(word_completion)
+            print("\n")
+        if guessed:
+            print("Congrats, you guessed teh word! You Win!")
+        else:
+            print("Sorry, you ran out of tries. The word was " + word + ".Maybe try next time!")
+
+def main():
+    word = get_word()
+    play(word)
+    while input("Play Again? (Y/N)").upper() == "Y":
+        word = get_word()
+        play(word)
+
+if __name__ == "__main__":
+    main()
