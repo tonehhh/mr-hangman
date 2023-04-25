0121359c4b690e6533f5dca462e16d5cae9cbc90
tonehhh <tonywhitmarsh93@gmail.com>
2023-04-25 10:20:42 +0000

    added display title and username

--- run.py
+++ run.py
@@ -146,6 +146,14 @@ def display_hangman(tries):
 
 
 def main():
+    print("""
+  __  __        _  _
+ |  \\/  |_ _   | || |__ _ _ _  __ _ _ __  __ _ _ _
+ | |\\/| | '_|  | __ / _` | ' \\/ _` | '  \\/ _` | ' \\
+ |_|  |_|_|(_) |_||_\\__,_|_||_\\__, |_|_|_\\__,_|_||_|
+                              |___/
+    """)
+    username = input ("Please enter your username: ")
     word = get_word()
     play(word)
     while input("Play Again? (Y/N)").upper() == "Y":
